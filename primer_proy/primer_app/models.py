6cf0dae068fa15ac4aa6dadaafe6d28265d35c5a
Hamilton <hamiltoncardona05@gmail.com>
2024-10-09 17:28:36 -0500

    9 de oct

--- primer_proy/primer_app/models.py
+++ primer_proy/primer_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from datetime import date
+from django.core.validators import MinValueValidator,MaxValueValidator
 # Create your models here.
 
 class Propietario(models.Model):
@@ -20,8 +21,87 @@ class Propietario(models.Model):
 #     nombre=models.CharField('Nombre de la mascota',max_length=100)
 #     especie=models.CharField('Especie de la m ascota',max_length=100)
 
+class Producto(models.Model):
+    codigo=models.CharField(verbose_name='Codigo del prodducto',
+                            max_length=100,
+                            unique=True,
+                            null=False)
+    nombre=models.CharField(verbose_name='Nombre del producto',
+                            max_length=100,
+                            null=False)
+    descripcion=models.CharField(verbose_name='Descripcion del producto',
+                                max_length=255,
+                                null=False)
+    presentacion=models.CharField(verbose_name='Presentacion del producto',
+                                max_length=100,
+                                null=False)
+    pais_origen=models.CharField(verbose_name='Pais de origen del producto',
+                                max_length=100,
+                                null=False)
+    activo=models.BooleanField(verbose_name='Producto activo #####',
+                            default=True,
+                            null=False)
+    fecha_fabric=models.DateField(verbose_name='Fecha de fabricacion del producto',
+                                default=date.today,
+                                null=False)
+    fecha_vencimineto=models.DateField(verbose_name='Fecha de vencimineto del producto',
+                                null=True)
+    precio=models.DecimalField(verbose_name='Precio del producto',
+                            max_digits=20,
+                            decimal_places=2)
+    stok=models.IntegerField(verbose_name='Stok del producto',
+                            default=0,
+                            validators=[MinValueValidator(0),MaxValueValidator(1000)],
+                            null=False)
 
-
-
-
-
+class Persona(models.Model):
+    codigo=models.CharField(verbose_name='Numero de documento',
+                            db_index=True,
+                            max_length=100,
+                            null=False)
+    nombre=models.CharField(verbose_name='Nombre de la perosna',
+                            max_length=100,
+                            null=False,
+                            db_index=True)
+    apellido=models.CharField(verbose_name='Apellido de la persona',
+                            max_length=150,
+                            null=False,
+                            help_text='Agrege su apellido')
+    ciudad=models.CharField(verbose_name='Ciudad de la persona',
+                            max_length=200,
+                            help_text='Ingrese su ciudad',
+                            default='Soacha',
+                            db_index=True,
+                            null=False)
+    direccion=models.CharField(verbose_name='Direccion de la persona',
+                            max_length=200,
+                            null=False,
+                            help_text='Ingrese su direccion')
+    telefono=models.CharField(verbose_name='Telefono de la persona',
+                            max_length=50,
+                            null=False,
+                            help_text='Ingrese su numero de telefono',
+                            unique=True)
+    correo=models.CharField(verbose_name='Correo de la persona',
+                            max_length=200,
+                            null=False,
+                            db_index=True,
+                            help_text='Ingrese su correo')
+    GENERO__OPC=[('M','Mssculino'),
+                ('F','Femenino')]
+    genero=models.CharField(verbose_name='Genero de la persona',
+                            max_length=1,
+                            choices=GENERO__OPC)
+    ACTIVO_OPC=[(1,'Activo'),
+                (2,'Inactivo')]
+    activo=models.PositiveIntegerField(verbose_name='Estado activo de la persona',
+                            choices=ACTIVO_OPC,
+                            default=True,
+                            help_text='Ingrese el estado de activida de la persona')
+    TIPO_PERSONA=[(0,'Natural'),(1,'Juridica')]
+    tipo_persona=models.CharField(verbose_name='Tipo de persona',
+                                max_length=20,
+                                null=False,
+                                help_text='Igrese si es persona natural y juridica',
+                                choices=TIPO_PERSONA,
+                                default='Natural')
